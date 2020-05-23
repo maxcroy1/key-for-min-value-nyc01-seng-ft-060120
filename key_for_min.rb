c98371217f10bb880d2d94393419e13ff8853b08
Maxwell Croy <max.croy1@gmail.com>
2020-05-23 17:05:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  low_num = 0
-  low_key = ""
+  low_key, low_num = name_hash.first
   name_hash.each do |key, value|
     if value < low
   end
