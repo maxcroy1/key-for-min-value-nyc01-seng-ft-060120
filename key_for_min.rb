ccbacb16e87a7ddb21ecbaf29dc3758d3d912a76
Maxwell Croy <max.croy1@gmail.com>
2020-05-23 17:06:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,9 @@
 def key_for_min_value(name_hash)
   low_key, low_num = name_hash.first
   name_hash.each do |key, value|
-    if value < low
+    if value < low_num
+      low_key = key
+    end
   end
   low_key
 end
