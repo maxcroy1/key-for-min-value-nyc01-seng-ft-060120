f6a1b337d0b8bb28ccf198688c19b95ce0e37656
Maxwell Croy <max.croy1@gmail.com>
2020-05-23 16:50:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  low_num = 0
+  low_key = ""
   name_hash.first do |key, value|
     low_num = value
     low_key = key
